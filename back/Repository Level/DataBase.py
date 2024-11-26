f4e19395300fd39bf9339d64817b901d5f8e3da9
loysoplayer <115035622+loysoplayer@users.noreply.github.com>
2024-11-26 08:41:03 +0300

    Update DataBase.py
    Rename functions, class, variables.
    Add exception handling

--- back/Repository Level/DataBase.py
+++ back/Repository Level/DataBase.py
@@ -2,30 +2,27 @@ from peewee import *
 
 db = SqliteDatabase('Users.db')
 
-class Users(Model):
-    adress = TextField(unique=True)
-    password = TextField()
+class User(Model):
+    e-mail = TextField(unique=True)
+    hash_password = TextField()
 
     class Meta:
         database = db # This model uses the "people.db" database.
 
-db.create_tables([Users])
+def log_in(e-mail, password):
+    try:
+        Users.create(adress =  adress, password = password)
+    except Exception as e:
+        return e
 
-def print_last_five_artists():
-    """ Печатаем последние 5 записей в таблице исполнителей"""
-    print('########################################################')
-    cur_query = Users.select().limit(5).order_by(Users.adress.desc())
-    for item in cur_query.dicts().execute():
-        print('user: ', item)
+def delete_user(e-mail):
+    try:
+        user = Users.get(Users.adress == e-mail)
+        user.delete_instance()
+    except Exception as e:
+        return e
 
-def create(adress, password):
-    Users.create(adress =  adress, password = password)
-
-def delete(adress):
-    user = Users.get(Users.adress == adress)
-    user.delete_instance()
-
-def find(adress):
-    return Users.get(Users.adress == adress)
+def sign_in(e-mail, password):
+    return Users.get(Users.adress == e-mail)
 
 db.close()
