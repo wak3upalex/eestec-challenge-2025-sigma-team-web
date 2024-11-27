47b91d02b9c0c8c0086ad3bd5e3f22c7dfa71725
loysoplayer <115035622+loysoplayer@users.noreply.github.com>
2024-11-27 13:12:08 +0300

    Add asociative for Level

--- back/Repository Level/DataBase.py
+++ back/Repository Level/DataBase.py
@@ -1,42 +1,68 @@
 from peewee import *
 import bcrypt
+from abc import ABC, abstractmethod
+from typing import List, TypeVar
 
-db = SqliteDatabase('Users.db')
+T = TypeVar('T')
 
-class User(Model):
-    e-mail = TextField(unique=True)
+
+mysql_db = MySQLDatabase('users')
+
+
+class DBase(ABC):
+    
+    @abstractmethod
+    def log_in(self, entity: T) -> None:
+        pass
+
+    @abstractmethod
+    def delete_user(self, e_mail: str) -> None:
+        pass
+
+    @abstractmethod
+    def sign_in(self, e_mail: str, entered_password: str) -> T:
+        pass
+
+    class Meta:
+             database = mysql_db    
+
+class User:
+    def __init__(self,e_mail, password):
+        self.e_mail = e_mail
+        self.password = password
+
+class UserRepository(DBase):
+    e_mail = TextField(unique=True)
     hash_password = TextField()
 
     class Meta:
-        database = db # This model uses the "people.db" database.
+        database = mysql_db # This model uses the "people.db" database.
 
-def log_in(e-mail, password):
-    salt = bcrypt.gensalt()
-    hash_password = bcrypt.hashpw(
-        password=my_password,
-        salt=salt
-    )
-    try:
-        Users.create(adress =  adress, hash_password = hash_password)
-    except Exception as e:
-        return e
-
-def delete_user(e-mail):
-    try:
-        user = Users.get(Users.adress == e-mail)
-        user.delete_instance()
-    except Exception as e:
-        return e
-
-def sign_in(e-mail, entered_password):
-    try:
-        user_password = Users.get(Users.adress == e-mail).password
-        check = bcrypt.checkpw(
-            password=entered_password,
-            hashed_password=user_password
+    def log_in(self, user: User) -> None:
+        salt = bcrypt.gensalt()
+        hash_password = bcrypt.hashpw(
+            password= user.password,
+            salt=salt
         )
-        return check
-    except Exception as e:
-        return e
+        try:
+            UserRepository.create(e_mail =  user.e_mail, hash_password = hash_password)
+        except Exception as e:
+            return e
 
-db.close()
+    def delete_user(self, e_mail: str) -> None:
+        try:
+            user = UserRepository.get(UserRepository.e_mail == e_mail)
+            user.delete_instance()
+        except Exception as e:
+            return e
+
+    def sign_in(self, e_mail, entered_password) -> User:
+        try:
+            user_password = UserRepository.get(UserRepository.e_mail == e_mail).password
+            check = bcrypt.checkpw(
+                password=entered_password,
+                hashed_password=user_password
+            )
+            return check
+        except Exception as e:
+            return e
