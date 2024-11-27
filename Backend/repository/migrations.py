bbf82709f12f6be815a58fa5000071395e403b5c
loysoplayer <115035622+loysoplayer@users.noreply.github.com>
2024-11-27 18:05:42 +0300

    Write code in migrations.py

--- Backend/repository/migrations.py
+++ Backend/repository/migrations.py
@@ -1,1 +1,15 @@
+from peewee import Model, CharField
+from repository.database import db
 
+class User(Model):
+    def __init__(self,e_mail, password):
+        e_mail = CharField(unique=True)
+        self.password = CharField()
+
+      class Meta:
+        database = db
+        table_name = 'users'
+
+def migrate():
+    with db:
+        db.create_tables([UserModel])
